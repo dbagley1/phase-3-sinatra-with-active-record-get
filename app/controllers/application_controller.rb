3570f757ac6a37cab49c34de9dd478860c9d4bd2
Darrian Bagley <dbagle14@gmail.com>
2022-06-18 14:07:08 -0400

    Completed assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
   get '/' do
     { message: "Hello world" }.to_json
   end
 
+  get '/games' do
+    Game.all.to_json
+  end
+
+  get '/games/:id' do
+    Game.find(params[:id]).to_json(include: { reviews: { include: :user } })
+  end
 end
